fb67c7e529096b0e3f273efee2fd52de6b4e7d3d
cristen <hunghoo@163.com>
2017-09-19 22:53:44 +0800

    基本完成

--- index.js
+++ index.js
@@ -35,15 +35,15 @@
     // ---------------------- 纯函数的定义 B -----------------------------------------
     /**
      * 判断数据的类型
-     * @param {*} val   
-     * @param {str} type  
+     * @param {*} val
+     * @param {str} type
      */
     function checkType(val, type) {
-        Object.prototype.toString.call(val) === "[object "+ type +"]"; 
+        Object.prototype.toString.call(val) === "[object " + type + "]";
     }
     /**
      * 判断是否是闰年
-     * @param {Number} year 
+     * @param {Number} year
      */
     function isLeep(year) {
         return (year % 100 !== 0 && year % 4 === 0) || (year % 400 === 0);
@@ -55,9 +55,9 @@
      */
     function getDaysNum(m, y) {
         var num = 31;
-        switch(m) {
+        switch (m) {
             case 2:
-                num =  isLeep(y) ? 29 : 28;
+                num = isLeep(y) ? 29 : 28;
                 break;
             case 4:
             case 6:
@@ -74,41 +74,144 @@
 
     // ---------------------- DOM的相关操作 E ----------------------------------------
 
-    // 日期组件的模板 B
-    var calenderTmpl = {
-        wrap: function() {
-            return '<div id="" class="jedatebox jedateblue" author="chen guojun" jeformat="YYYY-MM-DD hh:mm:ss" jefixed="#refix" style="position: relative; display: block;"></div>';
-        },
-        title: function() {
-            return '<div class="jedatetop">'+
-                        '<div class="jedateym" style="width:50%;">'+
-                            '<i class="prev triangle monthprev"></i>'+
-                            '<span class="jedatemm" ym="12">'+
-                                '<em class="jedatemonth" month="9">9月</em>'+
-                                '<em class="pndrop"></em>'+
-                            '</span>'+
-                            '<i class="next triangle monthnext"></i>'+
-                        '</div>'+
-                        '<div class="jedateym" style="width:50%;">'+
-                            '<i class="prev triangle yearprev"></i>'+
-                            '<span class="jedateyy" ym="24">'+
-                                '<em class="jedateyear" year="2017">2017年</em>'+
-                                '<em class="pndrop"></em>'+
-                            '</span>'+
-                            '<i class="next triangle yearnext"></i>'+
-                        '</div>'+
-                    '</div>'
-        },
-        content: function() {
+    var calenderFn = {};
+    calenderFn.getMonthData = function(year, month) {
+        var ret = []; //使用数组来获取一组需要的数据
 
-        },
-        foot: function() {
-
+        if (!year || !month) {
+            var now = new Date();
+            year = now.getFullYear();
+            month = now.getMonth();
         }
+        var firstDay = new Date(year, month - 1, 1);
+        year = firstDay.getFullYear();
+        month = firstDay.getMonth() + 1;
+        var weekNum = firstDay.getDay();
+        if (weekNum === 0) {
+            weekNum = 7;
+        }
+        var lastDayOfLastMonth = new Date(year, month - 1, 0);
+        var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
+        var preMonthCount = firstDay.getDay() - 1; // 第一天需要打头需要空几格
+        var lastDay = new Date(year, month, 0);
+        var lastDate = lastDay.getDate();
+        for (var i = 0; i < 6 * 7; i++) {
+
+            var date = i + 1 - preMonthCount; //
+            var showDate = date; //需要显示的日期
+            var thisMonth = month; // 当前的月份
+            // 上一个月
+            if (date <= 0) {
+                thisMonth = month - 1; //月份减一
+                showDate = lastDateOfLastMonth + date;
+            } else if (date > lastDate) {
+                // 下一个月
+                thisMonth = month + 1; // 月份加一
+                showDate = showDate - lastDate;
+            }
+            if (thisMonth === 0) {
+                thisMonth = 12;
+            }
+            if (thisMonth === 13) {
+                thisMonth = 1;
+            }
+            ret.push({
+                month: thisMonth,
+                date: date,
+                showDate: showDate
+            });
+        }
+        return {
+            year: year,
+            month: month,
+            days: ret
+        };
     }
+    var dateData = null;
+    // 日期组件的模板
+    calenderFn.createUI = function(year, month) {
+        dateData = calenderFn.getMonthData(year, month);
+        var monthData = dateData.days;
+        var month = dateData.month;
+        var year = dateData.year;
+        var html = '<div class="ui-calender-title">' +
+            '<a href="#" class="ui-calender-btn ui-canlender-prev">&lt;</a>' +
+            '<a href="#" class="ui-calender-btn ui-calender-next">&gt;</a>' +
+            '<span class="ui-calender-cur-month">' + year + '-' + month +
+            '</span>' +
+            '</div>' +
+            '<div class="ui-calender-content">' +
+            '<table>' +
+            '<thead>' +
+            '<tr>' +
+            '<th>一</th>' +
+            '<th>二</th>' +
+            '<th>三</th>' +
+            '<th>四</th>' +
+            '<th>五</th>' +
+            '<th>六</th>' +
+            '<th>日</th>' +
+            '</tr>' +
+            '</thead>' +
+            '<tbody>';
+        for (var i = 0, monLen = monthData.length; i < monLen; i++) {
+            var item = monthData[i];
+            if (i % 7 === 0) {
+                // 每周的第一天
+                html += '<tr>';
+            }
+            console.log(item);
+            html += '<td data-month=' + item.month + '>' + item.showDate +
+                '</td>'
+            if (i % 7 === 6) {
+                // 每周的最后一天
+                html += '</tr>';
+            }
+        };
+        html += '</tbody>' +
+            '</table>' +
+            '</div>';
+        return html;
+    }
+    var $wrapper
+    calenderFn.render = function(dire, $input) {
+        var year, month;
+        if (dateData) {
+            year = dateData.year;
+            month = dateData.month;
+        }
+        if (dire === "prev") {
+            month--;
+        } else if (dire === "next") {
+            month++;
+        }
+        var renderHtml = calenderFn.createUI(year, month);
+        $wrapper = document.createElement("div");
+        $wrapper.className = 'ui-calender-wrapper';
+        $wrapper.innerHTML = renderHtml;
+        $($input).after($($wrapper));
+    }
+    calenderFn.init = function($input) {
+        calenderFn.render(null, $input);
 
-    // 日期组件的模板 E
-
+        $($input).on("click", function() {
+            var left = $($input).offset().left;
+            var top = $($input).offset().top;
+            var height = $($input).height();
+            $($wrapper).css({
+                "top": top + height + 2,
+                "left": left
+            });
+            $($wrapper).show();
+        });
+        $($wrapper).on("click", ".ui-canlender-prev", function() {
+            calenderFn.render("prev", $input);
+        });
+        $($wrapper).on("click", ".ui-canlender-next", function() {
+            calenderFn.render("next", $input);
+        });
+    };
+    window.calenderFn = calenderFn;
     // --------------------------- 构造函数 B ------------------------------------
     var Calender = function() {
 
@@ -123,6 +226,5 @@
     // --------------------------- 构造函数 E ------------------------------------
 
     // -------------------------- 使用示例 B -----------------------------------
-
     // -------------------------- 使用示例 E -----------------------------------
 });
